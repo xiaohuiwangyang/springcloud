0298e475f0f21afc40a67cb2d572561b7a8da08b
wang <18815285397@163.com>
2020-03-03 09:41:54 +0800

    hellodemo

---
+++ hello-demo-peer1/src/main/java/com/dn/hellodemo/HelloDemoPeer1Application.java
@@ -0,0 +1,35 @@
+package com.dn.hellodemo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class HelloDemoPeer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDemoPeer1Application.class, args);
+    }
+
+
+    @GetMapping("")
+    public Object index() {
+        String str = "这是服务端1返回的应答";
+        return new String(str);
+    }
+
+
+}
